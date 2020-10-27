7952ff0fcdeceadfdf6e2e61f348819f8f81fbde
Juliana Andrews <julianakandrews@gmail.com>
2020-10-26 22:20:47 -0700

    minor changes

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,10 +26,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
 });
